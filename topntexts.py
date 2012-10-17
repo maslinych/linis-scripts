0db92acafaad675a5637dc71ecad2812567f9b6a
Kirill Maslinsky <kirill@altlinux.org>
2012-10-17 11:57:56 +0400

    fix some cleaning errors
        - fix topntexts.py
        - make topic-term-distributions.csv readable
        - update dist-lda target properly

--- topntexts.py
+++ topntexts.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 import os
-import sys
 import codecs
 import textwrap
 import re
+import argparse
 from collections import defaultdict
 
 # synopsis: <csv> <summary> <texts> <n> <outfile>
+aparser = argparse.ArgumentParser(description='Print top N texts for each topic based on LDA results')
+aparser.add_argument('csv', help='Main LDA results file (document-topics-distribution.csv)')
+aparser.add_argument('summary', help='File with topic top-words (summary.txt)')
+aparser.add_argument('texts', help='File with source texts suitable for reading (clean.txt)')
+aparser.add_argument('n', type=int, help='Number of top texts to print for each topic')
+aparser.add_argument('outfile', help='Output file')
+args = aparser.parse_args()
+
 
 dtd = defaultdict(list)
 topics = None
 texts = {}
 
-toptexts = int(sys.argv[4])
-
-with codecs.open(sys.argv[1], 'r', encoding='utf-8') as csv:
+with codecs.open(args.csv, 'r', encoding='utf-8') as csv:
     for line in csv:
         fields = line.strip('\n').split(',')
         if not topics:
@@ -23,10 +29,10 @@ with codecs.open(sys.argv[1], 'r', encoding='utf-8') as csv:
             dtd[t].append((float(fields[t+1]), str(fields[0])))
 
 
-with codecs.open(sys.argv[2], 'rb', encoding='utf-8') as txt:
+with codecs.open(args.summary, 'rb', encoding='utf-8') as txt:
     summaries = txt.read().strip('\n').split('\n\n\n')
 
-with codecs.open(sys.argv[3], 'r', encoding='utf-8') as clean:
+with codecs.open(args.texts, 'r', encoding='utf-8') as clean:
     for line in clean:
         m = re.search(ur'^(?P<textid>[0-9]+),(?P<text>.*)$', line)
         try:
@@ -37,11 +43,11 @@ with codecs.open(sys.argv[3], 'r', encoding='utf-8') as clean:
 for topic,values in dtd.items():
     values.sort(reverse=True)
 
-with codecs.open(sys.argv[5], 'wb', encoding='utf-8') as out:
+with codecs.open(args.outfile, 'wb', encoding='utf-8') as out:
     for topic,values in dtd.items():
         out.write("###################################################################################################\n")
         out.write(summaries[topic] + "\n")
-        for text in values[:toptexts]:
+        for text in values[:args.n]:
             out.write("\n\n")
             out.write(u'=== {0}, {1} ===\n'.format(text[1], text[0]))
             try:
